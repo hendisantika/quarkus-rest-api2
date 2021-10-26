aaed78544ab791c6c92cfeec92f684f232bf644a
Hendi Santika <hendisantika@gmail.com>
2021-10-27 06:59:29 +0700

    #17 - Add findAll in PersonResource

--- src/main/java/com/hendisantika/people/web/PersonResource.java
+++ src/main/java/com/hendisantika/people/web/PersonResource.java
@@ -1,8 +1,13 @@
 package com.hendisantika.people.web;
 
 import com.hendisantika.people.db.PersonRepository;
+import com.hendisantika.people.model.Person;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +26,16 @@ public class PersonResource {
     public PersonResource(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
+
+    // using `Response` requires extra code to enable runtime reflection
+    // this is important if running against GraalVM
+    // the serialization type can infer the serialization types at compile time
+    // meaning that nothing needs to be done when the code below is used
+    // maybe cover this in a separate post?
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Person> findAll() {
+        return personRepository.findAll();
+    }
+
 }
